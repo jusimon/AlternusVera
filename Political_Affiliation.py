b170e0c408299b469aaa3831f857bde09859a9dd
jusimon <julian05simon@gmail.com>
2019-12-08 12:00:05 -0800

    Updated Prediction

--- Political_Affiliation.py
+++ Political_Affiliation.py
@@ -1,28 +1,19 @@
-import numpy as np
-import pandas as pd
 import warnings
 import pickle
 
-labelcolname = 'Encoded_Label'
-titlecolname = 'partyAffiliation'
 def prediction(xtest, ytest):
+    xtest = xtest.split(' ')
     pickleModel = "/content/gdrive/My Drive/Drifters/Models/Politcal_Affiliation_Model.pkl"
     pickle_in = open(pickleModel, "rb")
-    loadData = pickle.load(pickle_in)
-    return np.mean(loadData.predict(xtest) == ytest)
-
-true_labels = ['original','true','mostly-true','half-true']
-false_labels = ['barely-true','false','pants-fire']
-def simplify_label(input_label):
-    if input_label in true_labels:
-        return 1
-    else:
-        return 0
-
+    loadData = pickle.load(pickle_in)   
+    dataset = loadData.predict(xtest)
+    for i in dataset:
+        if(i == 0):
+            return 0
+    return 1
+            
 class Political_Affiliation:
     def __init__(self, xtest):
-        self.x_test = xtest[titlecolname]
-        self.y_test = xtest.apply(lambda row: simplify_label(row['Label']), axis=1)
-        self.x_test = self.x_test.replace(np.nan,'No Job Title', regex=True)
+        self.x_test = xtest
     def predict(self):
-        return prediction(self.x_test, self.y_test)
+        return prediction(self.x_test)
