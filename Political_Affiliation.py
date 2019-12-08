d762dbba4ab80b55c2582bb57f8673e9628fe380
jusimon <julian05simon@gmail.com>
2019-12-07 19:55:13 -0800

    Label change

--- Political_Affiliation.py
+++ Political_Affiliation.py
@@ -11,10 +11,18 @@ def prediction(xtest, ytest):
     loadData = pickle.load(pickle_in)
     return np.mean(loadData.predict(xtest) == ytest)
 
+true_labels = ['original','true','mostly-true','half-true']
+false_labels = ['barely-true','false','pants-fire']
+def simplify_label(input_label):
+    if input_label in true_labels:
+        return 1
+    else:
+        return 0
+
 class Political_Affiliation:
     def __init__(self, xtest):
         self.x_test = xtest[titlecolname]
-        self.y_test = xtest[labelcolname]
+        self.y_test = xtest.apply(lambda row: simplify_label(row['Label']), axis=1)
         self.x_test = self.x_test.replace(np.nan,'No Job Title', regex=True)
     def predict(self):
         return prediction(self.x_test, self.y_test)
